8911520718ac2514b49c5cb9229ef2b42a4c6ff5
Luc Pitre <pitreluc@gmail.com>
2016-12-06 19:58:54 -0500

    added bouncing to p5js sketch

--- sessions/session2/sim/sketch.js
+++ sessions/session2/sim/sketch.js
@@ -6,13 +6,29 @@ function setup() {
   createCanvas(displayWidth, displayHeight);
   xpos = (displayWidth/2);
   ypos = (displayHeight/2);
-  speed = 2;
+  xspeed = 2;
+  yspeed = 2;
 }
 
 function draw() {
   background(0);
   fill(255);
+  
+  if(xpos >= displayWidth){
+  	xspeed*=1;
+  }else if(xpos < 0){
+  	xspeed*=1;
+  }
+  
+  if(ypos >= displayHeight){
+  	yspeed*=1;
+  }else if(ypos < 0){
+  	yspeed*=1;
+  }
+  
   ellipse(xpos, ypos, 20, 20);
-  xpos-=speed;
-  ypos-=speed;
+  xpos+=speed;
+  ypos+=speed;
+  
+  
 }
