d16d7c0a9d14b44f1abb040558a139ce58fe80c0
Luc Pitre <pitreluc@gmail.com>
2016-12-06 20:03:38 -0500

    Update sketch.js

--- sessions/session2/sim/sketch.js
+++ sessions/session2/sim/sketch.js
@@ -16,15 +16,15 @@ function draw() {
   fill(255);
   
   if(xpos >= displayWidth){
-  	xspeed*=1;
+  	xspeed*=-1;
   }else if(xpos < 0){
-  	xspeed*=1;
+  	xspeed*=-1;
   }
   
   if(ypos >= displayHeight){
-  	yspeed*=1;
+  	yspeed*=-1;
   }else if(ypos < 0){
-  	yspeed*=1;
+  	yspeed*=-1;
   }
   
   ellipse(xpos, ypos, 20, 20);
